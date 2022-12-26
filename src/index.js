39fbbd61e43941d7412dfb23289b613a86c31746
Achsmboh <maulana.achsanxiipa1@gmail.com>
2022-12-26 11:21:44 +0700

    slicing page categories

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.css";
-import App from "./pages/App";
-import Dashboard from "./pages";
-import Categories from "./pages/Categories";
+import App from "./routes";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Categories />
+    <App />
   </React.StrictMode>
 );
