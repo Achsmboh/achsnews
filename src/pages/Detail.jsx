f8c646d681fa4a6f6afaaa3438a0a2f58200df2e
Achsmboh <maulana.achsanxiipa1@gmail.com>
2022-12-26 11:38:53 +0700

    slicing page detail

---
+++ src/pages/Detail.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+function Detail() {
+  return (
+    <Layout>
+      <div className="h-96 w-full bg-yellow-400">
+        <img className="w-full h-full object-cover object-top" src="https://tse2.mm.bing.net/th?id=OIP.BkkfzHNYZAD8u0X51EN2PwHaFa&pid=Api&P=0" alt="foto" />
+      </div>
+      <div className="py-3">
+        <h2 className="text-brown font-semibold font-Inter text-3xl">Judul Berita</h2>
+      </div>
+      <div>
+        <p className="text-brown font-Inter text-sm">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed fuga quod dolorum voluptatum tempore libero vero mollitia. Expedita fugiat in, quibusdam doloribus velit incidunt? Inventore sequi dolorum accusantium molestias nulla,
+          sed dignissimos deleniti velit odio, quod natus et dolor, earum quo pariatur cum! Illo, tempora! Nobis at a totam nihil fugit. Omnis inventore veniam, suscipit illum fuga ratione? Labore dolorum vero, veniam aliquid adipisci ipsam
+          nobis ducimus praesentium iusto enim, eum quia error esse, tenetur qui illo? Dolor pariatur porro architecto, quae similique odit quaerat adipisci unde eaque ipsum ex, praesentium consequatur deleniti aliquid fuga blanditiis vel
+          placeat! Ad sequi, culpa ab laborum voluptas accusamus ullam voluptates reiciendis adipisci veritatis dolor dicta. Assumenda aut cumque incidunt voluptatibus sunt iste impedit fugiat porro quod numquam inventore voluptates,
+          laborum a minus soluta perspiciatis amet quo aperiam iusto dolorum vitae eligendi autem facilis omnis. Quaerat corporis aliquid, animi quae temporibus itaque doloribus perspiciatis officia dolor? Sint expedita iusto quidem minima
+          possimus inventore nisi omnis deserunt sed aut, similique ipsum unde non ipsa. Assumenda deserunt voluptatem hic dolorem pariatur neque maxime culpa quidem porro molestias, dicta mollitia doloremque aspernatur consequuntur
+          quisquam cumque aliquam obcaecati. Accusantium atque officia id vel nemo sint aperiam iusto est. Ea, esse exercitationem molestias aliquid, aut amet facilis dicta ad recusandae dolore repudiandae. Reprehenderit atque expedita
+          eligendi molestiae, dolorem, ipsum temporibus nesciunt perspiciatis ut saepe fugit soluta vel quos debitis, officia nulla at aspernatur. Distinctio natus quae quam, fugiat, temporibus recusandae, nobis itaque molestiae quas velit
+          unde illo ducimus magnam laboriosam excepturi quod eligendi maxime ullam harum accusantium et nam! Modi cum, exercitationem odit, fuga vitae ex eos aliquam voluptates, inventore officiis quasi quo id! Quibusdam rem aliquam esse,
+          voluptatem cupiditate sit alias tempora suscipit consectetur ea, numquam fuga officiis doloremque qui, illo maxime quod doloribus expedita quia aperiam nam. Tenetur, ipsum libero asperiores consectetur magni laudantium commodi
+          tempore blanditiis possimus dolorem officia, consequuntur nemo eveniet enim eaque repellat aliquid ipsa perspiciatis excepturi, perferendis aut deleniti vitae necessitatibus reprehenderit? Laboriosam corrupti quo similique
+          laborum, accusamus minus explicabo necessitatibus molestias libero dicta natus! Illum totam, molestiae corporis nesciunt repellat cupiditate eius quibusdam quidem provident nulla commodi, sed mollitia pariatur? Unde eos a fuga
+          veritatis aspernatur omnis obcaecati iure adipisci ad cumque excepturi inventore necessitatibus impedit ab maxime eius facilis, pariatur perferendis aut nostrum vero! Voluptatem aliquid corrupti dolorem at, consequatur minus porro
+          explicabo laboriosam id sit tempore voluptates? Unde rerum vel, consequuntur dolor iste dolores enim temporibus provident aliquam esse facilis soluta. Provident hic beatae amet molestiae possimus numquam inventore, dolorum
+          cupiditate debitis corporis corrupti qui ex doloremque? Repellat, similique corporis. Distinctio doloribus earum ut at optio soluta officiis nisi odio eius ipsa architecto adipisci harum exercitationem quae quia voluptas fugit
+          obcaecati iste, non modi beatae sequi possimus! Quibusdam itaque ipsa debitis reiciendis consectetur. Facilis, illo. Quas quam non est? Voluptates, voluptas! Dolorem quasi aut repellendus debitis nisi expedita nulla sapiente dicta
+          maiores quas numquam sed possimus consequatur rem alias velit repudiandae, quae nostrum veritatis quam. Aspernatur nam maiores pariatur dicta.
+        </p>
+      </div>
+    </Layout>
+  );
+}
+
+export default Detail;
